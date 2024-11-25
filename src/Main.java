a35dfda51b52a4acb8f86529898a3b9b9ca2563a
asus2 <izmailoff.ehduard@yandex.ru>
2024-11-25 12:44:20 +0300

    Новое

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание№1");
+        System.out.println("Задание№1.");
 
         int age = 18;
         if (age >= 18) {
